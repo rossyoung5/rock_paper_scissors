72b9d7a908eb2dd55ca5287023e9d344584256a8
Ross Young <ross.young12@gmail.com>
2016-02-03 11:39:57 -0600

    adds index and result page

--- app.rb
+++ app.rb
@@ -11,3 +11,11 @@ helpers RockPaperScissorsHelper
 get '/' do
   erb :index
 end
+
+post '/result' do
+  human_move=params[:move]
+  #binding.pry
+  computer_move=computer_selection
+  winner = get_winner(human_move, computer_move)
+  erb :result, locals: {game_result:winner}
+end
--- helpers/rps.rb
+++ helpers/rps.rb
@@ -10,17 +10,6 @@ module RockPaperScissorsHelper
     ['scissors', 'paper']
   ]
 
-  def play
-    human_move = human_selection
-    computer_move = computer_selection
-    get_winner(human_move, computer_move)
-  end
-
-  def human_selection
-    puts "Rock, paper, or scissors?"
-    gets.chomp.downcase
-  end
-
   def computer_selection
     PLAYS.sample
   end
@@ -29,12 +18,12 @@ module RockPaperScissorsHelper
 
   def get_winner(human_move, computer_move)
     if human_move == computer_move
-      puts "It's a draw, two #{human_move.capitalize}s... play again!"
+      "It's a draw, two #{human_move.capitalize}s... play again!"
     else
       if WINS.include?([human_move, computer_move])
-      puts "You win! #{human_move.capitalize} beats #{computer_move.capitalize}"
+      "You win! #{human_move.capitalize} beats #{computer_move.capitalize}"
       else
-      puts "You lose...#{computer_move.capitalize} beats #{human_move.capitalize}"
+      "You lose...#{computer_move.capitalize} beats #{human_move.capitalize}"
       end
     end
   end
